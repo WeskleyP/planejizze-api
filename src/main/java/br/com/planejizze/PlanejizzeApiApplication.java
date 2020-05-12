cdcf47e0a66c8ec6f004970a8795364f415283d4
WeskleyP <weskleypedro@gmail.com>
2020-05-12 20:38:02 -0300

    first commit

---
+++ src/main/java/br/com/planejizze/PlanejizzeApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.planejizze;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PlanejizzeApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(PlanejizzeApiApplication.class, args);
+    }
+
+}
