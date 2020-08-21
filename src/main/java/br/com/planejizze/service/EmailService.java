d1fd082a4d93b2b1a21dbf94fd4e6319144074c2
WeskleyP <weskleypedro@gmail.com>
2020-08-21 19:21:03 -0300

    removendo credenciais

---
+++ src/main/java/br/com/planejizze/service/EmailService.java
@@ -0,0 +1,29 @@
+package br.com.planejizze.service;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+public class EmailService {
+
+    private final JavaMailSender emailSender;
+    @Value("${default.sender}")
+    private String sender;
+
+    @Autowired
+    public EmailService(JavaMailSender emailSender) {
+        this.emailSender = emailSender;
+    }
+
+    public void sendMessage(String to, String subject, String text) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(sender);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        emailSender.send(message);
+    }
+}
